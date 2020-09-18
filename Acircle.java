937d77eb6dcdba599e2c9b839382d26c27dffc15
pranav gaikwad <70343216+pranav808@users.noreply.github.com>
2020-09-18 23:07:53 +0530

    Add files via upload

---
+++ Acircle.java
@@ -0,0 +1,13 @@
+class Acircle
+{
+public static void main(String args[])
+  {
+int r;
+float pi=3.14f,a;
+r=Integer.parseInt(args[0]);
+System.out.println("Enter a radius "+r);
+a=pi*r*r;
+System.out.println("Area of circle is "+a);
+   }
+
+}
